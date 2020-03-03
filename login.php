975c2b5464145c1716334bf2f10b41150baae480
Danial Noaein <rbkk.noaein@gmail.com>
2020-03-03 22:05:59 +0330

    Move auth works to login.php

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+http_response_code(200);
+
+if ( array_key_exists("PHP_AUTH_USER", $_SERVER) &&  array_key_exists("PHP_AUTH_PW", $_SERVER) ) {
+    echo "thisisasimpletoken";
+}
+
+if ( array_key_exists("HTTP_TOKEN", $_SERVER) ) {
+    echo "tokenissended";
+}
+
+
+?>
