566054f23463842c0e50c3b7517a948e917f89b7
Danial Noaein <rbkk.noaein@gmail.com>
2020-03-04 17:15:00 +0330

    Show nothing message when no data sended

--- login.php
+++ login.php
@@ -8,11 +8,14 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 http_response_code(200);
 
 if ( array_key_exists("PHP_AUTH_USER", $_SERVER) &&  array_key_exists("PHP_AUTH_PW", $_SERVER) ) {
-    echo "thisisasimpletoken";
-}
 
-if ( array_key_exists("HTTP_TOKEN", $_SERVER) ) {
-    echo "tokenissended";
+    echo json_encode( array( 'username' => $_SERVER["PHP_AUTH_USER"],'password' => $_SERVER["PHP_AUTH_PW"] ) );
+
+}else if ( array_key_exists("HTTP_TOKEN", $_SERVER)  ) {
+	    echo "tokenissended";
+
+}else{
+	echo "NOTHING";
 }
 
 
